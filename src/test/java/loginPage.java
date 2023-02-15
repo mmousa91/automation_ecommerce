abacde2a0e8268b709318f608041b82b6f4c6f33
mmousa91 <109099345+mmousa91@users.noreply.github.com>
2023-02-15 18:03:00 +0200

    day2

--- src/test/java/loginPage.java
+++ src/test/java/loginPage.java
@@ -1,4 +1,7 @@
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
@@ -7,6 +10,9 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+
+import java.io.File;
+import java.io.IOException;
 
 public class loginPage {
 
@@ -24,7 +30,7 @@ driver.manage().window().maximize();
 }
 
 @Test
-public void Case1  () throws InterruptedException {
+public void Case1  () throws InterruptedException, IOException {
 //1- go to http://live.techpanda.org/index.php/
     driver.navigate().to("http://live.techpanda.org/index.php/");
 
@@ -55,7 +61,10 @@ public void Case1  () throws InterruptedException {
 
     //6- verify all product sorted by name
     soft.assertTrue(driver.findElement(By.xpath("//option[@value=\"http://live.techpanda.org/index.php/mobile.html?dir=asc&order=name\"]")).isDisplayed(),"not displayed");
-
+    File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+    String day1 = "day1";
+    String png = ( "C:\\Users\\MMousa\\Downloads" +day1+".png");
+    FileUtils.copyFile(scrFile,new File(png));
 
 
 
@@ -63,6 +72,34 @@ public void Case1  () throws InterruptedException {
 
 
     soft.assertAll();
+}
+
+@Test
+public void Day2 (){
+    //1- go to http://live.techpanda.org/index.php/
+    driver.navigate().to("http://live.techpanda.org/index.php/");
+
+    //2-click on mobile menu
+    driver.findElement(By.xpath("//a[@class=\"level0 \"]")).click();
+
+    // 3- read the cost of sony mobile and note this value
+    String value1=driver.findElement(By.id("product-price-1")).getText();
+    System.out.println(value1);
+
+    // 4 - click on sony xperia mobile
+    driver.findElement(By.linkText("SONY XPERIA")).click();
+
+    // 5- read the sony xperia mobile from details
+    String value2 = driver.findElement(By.cssSelector("span[class=\"price\"]")).getText();
+    System.out.println(value2);
+
+    // 6 - compare value 3 with value 5
+    SoftAssert soft = new SoftAssert();
+    soft.assertEquals(value1,value2,"the cost doesn't match");
+    soft.assertAll();
+
+
+
 }
 
 @AfterTest
